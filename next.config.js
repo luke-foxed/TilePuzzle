6e3d0a1828407b2b8469a63ee139af5578d5bd48
Luke Fox <lukefox5@gmail.com>
2022-12-28 14:58:45 +0000

    potential fix for build error

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const nextConfig = {
   staticPageGenerationTimeout: 1000,
 }
 
+// fix for ZLIB error (https://github.com/ivansevillaa/use-next-blurhash/issues/4)
+if (
+  process.env.LD_LIBRARY_PATH == null
+  || !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
+    process.env.LD_LIBRARY_PATH || ''
+  }`
+}
+
 module.exports = nextConfig
